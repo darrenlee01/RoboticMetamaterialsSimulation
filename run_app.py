460cb4eeb6f5a4fab86af34a10f7f32219b83fd3
caleboh2003 <caleboh2003@gmail.com>
2023-02-13 22:24:14 -0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -4,9 +4,16 @@ import pymunk.pygame_util
 import math
 
 pygame.init()
+space = pymunk.Space()
 
 width, height = 800, 800
 window = pygame.display.set_mode((width, height))
+
+class RotaryLimitJoint:
+    def __init__(self, b, b2, min, max, collide=True):
+        joint = pymunk.constraint.RotaryLimitJoint(b, b2, min, max)
+        joint.collide_bodies = collide
+        space.add(joint)
 
 def run (window, width, height):
     run = True
